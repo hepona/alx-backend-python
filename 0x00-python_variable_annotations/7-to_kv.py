3d1341318f6ad4bc367d792d76645acd68b3f827
hepona <hepona10@gmail.com>
2024-05-02 20:25:07 +0100

    7+9+10

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ from typing import Union, Tuple
 def to_kv(
     k: str,
     v: Union[int, float],
-) -> Tuple[str, Union[str, float]]:
+) -> Tuple[str, float]:
     """takes a string k and an int OR
     float v as arguments and returns a tuple."""
     return (k, v**2)
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """element length"""
-from typing import List, Tuple, Any
-from collections.abc import Iterable
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: Iterable) -> Tuple[Any]:
-    """doc"""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Annotate the below function's parameters and
+    return values with the appropriate types"""
     return [(i, len(i)) for i in lst]

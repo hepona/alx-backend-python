db1b1f3bebf693c7b1af1b11ce79e11189d36494
hepona <hepona10@gmail.com>
2024-05-02 18:29:03 +0100

    5...8

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """sum list"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """doc"""
     return sum(input_list)
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""type-annotated function make_multiplier that takes a float multiplier
+as argument and returns a function that multiplies a float by multiplier.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """doc"""
+
+    def func(n: float) -> float:
+        return n * multiplier
+
+    return func

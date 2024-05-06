eb7c0363005b6b811af7f248ac9b026b2b0c68fd
hepona <hepona10@gmail.com>
2024-05-06 10:47:55 +0100

    1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,9 +7,7 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 async def wait_n(n, max_delay):
     """spawn wait_random n times with the specified max_delay."""
-    lst = []
-    for _ in range(1, n):
-        data = wait_random(max_delay)
-        await asy.sleep(data)
-        lst.append(data)
-    return lst
+    data = []
+    for _ in range(n):
+        data.append(await asy.gather(wait_random(max_delay)))
+    return data

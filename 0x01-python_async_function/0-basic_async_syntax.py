6cb9fe1cf4aa69942aa27b1285c7f863d455ba1c
hepona <hepona10@gmail.com>
2024-05-06 10:08:07 +0100

    0+1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,9 +5,9 @@ import asyncio
 from typing import Union
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay=10):
     """takes in an integer argument
-    that waits for a random delay between 0 and"""
+    that waits for a random delay between 0 and max_delay"""
     wait = random.randrange(0, max_delay)
     await asyncio.sleep(wait)
     return wait

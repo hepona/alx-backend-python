a8b27b6a0dc08f6b9da37fc3e5a283d8d3029393
hepona <hepona10@gmail.com>
2024-05-07 14:55:23 +0100

    0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """async generator"""
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """coroutine will loop 10 times, each time asynchronously
     wait 1 second, then yield a random number between 0 and 10"""
     for _ in range(10):

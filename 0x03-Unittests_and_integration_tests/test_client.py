c1c91dd6c7064e6d591ae3b0d7a3fcd39c7ef07a
Rahif hiba <57334931+hepona@users.noreply.github.com>
2024-05-28 02:28:19 +0100

    9

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+""" test for client"""
+
+from client import GithubOrgClient
+import unittest
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """test for client"""
+
+    def test_public_repos(self):
+        """test public repos func"""
+
+        client = GithubOrgClient("google")
+        self.assertEqual(type(client.public_repos()), list)
